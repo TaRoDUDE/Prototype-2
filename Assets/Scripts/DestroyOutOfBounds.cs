ca7a3b35420fecdd054e42c44736010166512526
TaRo <swissmic0@gmail.com>
2023-04-20 14:18:05 -0400

    2.4 Done

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -18,10 +18,13 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        }
+        else if (transform.position.z < lowerBound)
+
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
-        
+
     }
 }
